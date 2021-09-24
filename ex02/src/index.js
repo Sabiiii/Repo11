0d5e759a36b0854a723ac06780655dae50244af6
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-24 09:22:00 +0400

    Adding muli dimensional arrays

---
+++ ex02/src/index.js
@@ -0,0 +1,29 @@
+var temps = [
+  monday = [31, 32, 19, 37],
+  tuesday = [29, 27, 55, 36],
+  wednesday = [17, 27, 42, 46],
+  thursday = [29, 52, 21, 64],
+  friday = [91, 27, 31, 61]
+];
+
+
+function myArrayFunction(arr) {
+
+    var newTemps = [...arr];
+    var averageDayTemp = [];
+
+    newTemps = temps[];
+
+    function index(arr) {
+    for (var i=0; i<arr.length; i++) {
+    for (var j=0; j<arr[i].length; j++) {
+    if (arr[i][j] == id) { return i; }
+    }
+}
+
+    return averageDayTemp;
+}
+
+console.log(myArrayFunction(temps));
+module.exports = myArrayFunction;
+
